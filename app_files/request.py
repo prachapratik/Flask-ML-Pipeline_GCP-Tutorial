1ba167a8d40b16116185c0045ed2ed4da5d75dd3
dpaul0501 <63039644+dpaul0501@users.noreply.github.com>
2022-02-13 02:10:14 +0530

    Update request.py

--- app_files/request.py
+++ app_files/request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+r = requests.post(url,json={'R&DSpend':2, 'Administration':9, 'Marketing':6})
 
 print(r.json())
